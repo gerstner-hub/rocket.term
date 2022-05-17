24d82a7fff2a6f346a68e78c79e6d3e8a65798e5
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2022-05-17 22:42:34 +0200

    setup.py: adjust version number to release tag

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ try:
 
     setup(
         name = 'rocket.term',
-        version = '0.2.0-r2',
+        version = '0.3.0-r1',
         description = 'rocket.term is a text based chat client for the Rocket.chat messaging solution',
         long_description = long_desc,
         author = 'Matthias Gerstner',
