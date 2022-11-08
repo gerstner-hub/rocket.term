1340f866512685bec035cf58874cd4bdc6ceb494
Matthias Gerstner <Matthias.Gerstner@nefkom.net>
2022-11-08 22:53:42 +0100

    module_helper: insert local module directory in front
    If a system or user based installation of rocket.term already exists
    then these modules will be prefered to the local modules currently. To
    avoid that inconsistency insert the local path in front.

--- bin/module_helper.py
+++ bin/module_helper.py
@@ -8,7 +8,7 @@ import pkgutil
 
 def tryFindModule(module):
     parent_dir = os.path.realpath(os.path.dirname(os.path.dirname(__file__)))
-    sys.path.append(parent_dir)
+    sys.path.insert(0, parent_dir)
 
     if pkgutil.find_loader(module) is None:
         print("The module {} could not be found in '{}'!".format(
